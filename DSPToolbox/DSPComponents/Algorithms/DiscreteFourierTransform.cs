437ca36ec2b746acc219c75bbf1833cd5f66a31b
Arwa Alaa El-din <arwaalaaeldin@gmail.com>
2022-11-04 16:10:20 +0200

    Update Discreate Fourier Transform

--- DSPToolbox/DSPComponents/Algorithms/DiscreteFourierTransform.cs
+++ DSPToolbox/DSPComponents/Algorithms/DiscreteFourierTransform.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DSPAlgorithms.DataStructures;
+using System.Numerics;
 
 namespace DSPAlgorithms.Algorithms
 {
@@ -15,7 +16,35 @@ namespace DSPAlgorithms.Algorithms
 
         public override void Run()
         {
-            throw new NotImplementedException();
+            OutputFreqDomainSignal = new Signal(new List<float>(), false);
+            int sz = InputTimeDomainSignal.Samples.Count;
+
+            List<float> Amplitudes = new List<float>();
+            List<float> PhaseShifts = new List<float>();
+            List<float> Frequencies = new List<float>();
+
+            for (int k=0 ; k < sz ; k++)
+            {
+                Complex sum = new Complex();
+                for (int n = 0 ; n<sz ; n++)
+                {                
+                    float ceta = ( k * n * 2 * (float)Math.PI) / sz;
+                    if (ceta == 0)
+                        sum += InputTimeDomainSignal.Samples[n];
+                    else
+                        sum +=(InputTimeDomainSignal.Samples[n] * new Complex(Math.Cos(ceta),  Math.Sin(ceta)*(-1) ));
+                }
+
+                Frequencies.Add(k);
+                Amplitudes.Add((float)(sum.Magnitude));
+                PhaseShifts.Add((float)sum.Phase);
+                
+            }
+
+            OutputFreqDomainSignal.FrequenciesAmplitudes = Amplitudes;
+            OutputFreqDomainSignal.FrequenciesPhaseShifts = PhaseShifts;
+            OutputFreqDomainSignal.Frequencies = Frequencies;
+
         }
     }
 }
