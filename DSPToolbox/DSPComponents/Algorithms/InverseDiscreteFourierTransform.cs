cee203b32c8f325f87bbcb7e3dfdb67d4ac9b786
huda <hsamy0706@gmail.com>
2022-11-19 10:14:45 +0200

    Task3

--- DSPToolbox/DSPComponents/Algorithms/InverseDiscreteFourierTransform.cs
+++ DSPToolbox/DSPComponents/Algorithms/InverseDiscreteFourierTransform.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DSPAlgorithms.DataStructures;
+using System.Numerics;
 
 namespace DSPAlgorithms.Algorithms
 {
@@ -14,7 +15,37 @@ namespace DSPAlgorithms.Algorithms
 
         public override void Run()
         {
-            throw new NotImplementedException();
+            
+            int k = InputFreqDomainSignal.Frequencies.Count;
+            Complex[] components = new Complex[k];
+
+            for (int i = 0; i < k; i++)
+            {
+                float A = InputFreqDomainSignal.FrequenciesAmplitudes[i];
+                float PhaseShift = InputFreqDomainSignal.FrequenciesPhaseShifts[i];
+                components[i] = new Complex(A * Math.Cos(PhaseShift), A * Math.Sin(PhaseShift));
+                
+            }
+            List<float> samples = new List<float>();
+            for (int i = 0; i < k; i++)
+            {
+                Complex sum = new Complex();
+                for (int j = 0; j < k; j++)
+                {
+                    float ceta = (i * j * 2 * (float)Math.PI) / k;
+                    if (ceta == 0)
+                        sum += components[j];
+                    else
+                        sum += (components[j] * new Complex(Math.Cos(ceta), Math.Sin(ceta)));
+
+                    
+                }
+                
+                samples.Add((float)(sum.Real)/k);
+                //Console.WriteLine(samples[i]);
+            }
+
+            OutputTimeDomainSignal = new Signal(samples, false);
         }
     }
 }
